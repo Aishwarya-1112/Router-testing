da2e5869b634f79a20b88b78c2e419a20162ede5
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-03-11 13:36:44 +0530

    Completed

---
+++ router-testing/src/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Welcome to my About Page</div>;
+};
+
+export default About;
---
+++ router-testing/src/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <div>Welcome to my Contact Page</div>;
+};
+
+export default Contact;
